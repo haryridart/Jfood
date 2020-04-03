66b15e988a1ce92c3426d3f72e28b86b920c81ef
haryridart <hary.teguh@ui.ac.id>
2020-04-03 19:49:16 +0700

    Modul6 - Post Test

--- src/Jfood.java
+++ src/Jfood.java
@@ -11,7 +11,7 @@ import java.util.Date;
 public class Jfood {
     // instance variables - replace the example below with your own
     public static void main(String[] args) {
-        Location locationObj = new Location("Sukabumi", "Jawa Barat", "Rumah Kita Semua");
+        /*Location locationObj = new Location("Sukabumi", "Jawa Barat", "Rumah Kita Semua");
         DatabaseSeller.addSeller((new Seller(DatabaseSeller.getLastId()+1 , "Narumi", "narumi@gmail.com", "085285237993", locationObj)));
         DatabaseCustomer.addCustomer(new Customer(DatabaseCustomer.getLastId(), "Hary", "hary@gmail.com", "hary1", new GregorianCalendar(2020, 4, 2)));
         DatabaseCustomer.addCustomer(new Customer(DatabaseCustomer.getLastId(), "Hary", "hary@gmail.com", "hary2", new GregorianCalendar(2020, 5, 2)));
@@ -28,11 +28,21 @@ public class Jfood {
         for (int i = 0; DatabaseFood.getFoodByCategory(FoodCategory.Beverages).size() > i; i++){
 
             System.out.println(DatabaseFood.getFoodByCategory(FoodCategory.Beverages).get(i).getName());
+        }*/
+
+        DatabasePromo.addPromo((new Promo(DatabasePromo.getLastId()+1,"SALAHLOH", 2000,15000,false))))
+        DatabasePromo.addPromo((new Promo(DatabasePromo.getLastId()+1,"BENERLOH", 5000,17000,true))))
+
+        for (Promo promo : DatabasePromo.getPromoDatabase()){
+            System.out.println(promo);
         }
 
-        //DatabasePromo.addPromo((new Promo(DatabasePromo.getLastId(),DatabasePromo.getPromoByCode(0),)))
+        DatabaseFood.addFood(new Food(DatabaseFood.getLastId()+1,"Mozarella", DatabaseSeller.getSellerById(DatabaseSeller.getLastId()),10000,FoodCategory.Beverages));
+        DatabaseFood.addFood(new Food(DatabaseFood.getLastId()+1,"Sate", DatabaseSeller.getSellerById(DatabaseSeller.getLastId()),22000,FoodCategory.Beverages));
+
     }
 }
+
 
 
 
